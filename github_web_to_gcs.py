982fbc5720e92ddf0563e9f98bf4a60db4cf53a5
diego <osocordoba@gmail.com>
2023-02-05 17:58:06 -0300

    .

--- github_web_to_gcs.py
+++ github_web_to_gcs.py
@@ -26,7 +26,7 @@ def write_local(df: pd.DataFrame, color: str, dataset_file: str) ->Path:
   """Write DataFrame out locally as parquet file"""
  
  #Added creation of the data/color directory:
- #Path(f"data/{color}").mkdir(parents=True, exist_ok=True)
+  Path(f"data/{color}").mkdir(parents=True, exist_ok=True)
 
   path = Path(f'data/{color}/{dataset_file}.parquet')
   df.to_parquet(path, compression='gzip')
